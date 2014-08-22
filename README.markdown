b527e363a05fa24893704de58ae6e94650ed5eb5
Anshuman Biswas <anchoo2kewl@gmail.com>
2014-08-22 12:22:49 -0400

    Changes to Rupa's Code to point to my personal URL

--- README.markdown
+++ README.markdown
@@ -1,19 +1,11 @@
-Command line pastebin for google app-engine, in use at [sprunge.us](http://sprunge.us).
+Command line pastebin for google app-engine, in use at [dbinpaste.appspot.com](http://dbinpaste.appspot.com).
 
 Requirements:
 
 * [pygments](http://pygments.org/). currently using version 1.6.
 * [cloudstorage](https://developers.google.com/appengine/docs/python/googlecloudstorageclient/download).
 
-Version 2:
 
-* Pretty much a complete rewrite, should be transparent to users.
-* Sprunge contents are stored in the Blobstore (actually the Cloud Storage
-  "default bucket") rather than the Datastore. This gives us more room.
-* Existing sprunges are migrated on demand.
-
-Version 1:
-
-* [For reference](https://github.com/rupa/sprunge/releases/tag/v1).
+Forked from [github.com/rupa/sprunge](Rupa).
 
 Licensed under the WTFPL.
--- sprunge.py
+++ sprunge.py
@@ -1,36 +1,32 @@
-import logging
+import cgi
+import os
 import random
-import urllib
-import webapp2
+import sys
+import wsgiref.handlers
 
+from google.appengine.ext import webapp
+from google.appengine.ext import db
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+import pygments.lexers
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
-import pygments.lexers
 
-import cloudstorage as gcs
-from google.appengine.api.app_identity import get_default_gcs_bucket_name
-from google.appengine.ext import blobstore, db
-from google.appengine.ext.webapp import blobstore_handlers
+class Sprunge(db.Model):
+    name = db.StringProperty()
+    content = db.TextProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
 
-URL = 'http://sprunge.us'
-POST = 'sprunge'
+class Index(webapp.RequestHandler):
 
-def new_id():
-    nid = ''
-    symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    while len(nid) < 4:
-        n = random.randint(0, 35)
-        nid = nid + symbols[n:n + 1]
-    return nid
+    u = 'http://dbinpaste.appspot.com'
+    r = 'ansh'
 
-def help():
-    form = (
-        'data:text/html,<form action="{0}" method="POST">'
-        '<textarea name="{1}" cols="80" rows="24"></textarea>'
-        '<br><button type="submit">{1}</button></form>'.format(URL, POST)
-    )
-    return """
-<style> a {{ text-decoration: none }} </style>
+    def help(self, u, r):
+        f = 'data:text/html,<form action="%s" method="POST"><textarea name="%s" cols="80" rows="24"></textarea><br><button type="submit">%s</button></form>' % (u, r, r)
+        return """
+<style> a { text-decoration: none } </style>
 <pre>
 sprunge(1)                          SPRUNGE                          sprunge(1)
 
@@ -38,122 +34,92 @@ NAME
     sprunge: command line pastebin.
 
 SYNOPSIS
-    &lt;command&gt; | curl -F '{0}=&lt;-' {1}
+    &lt;command&gt; | curl -F '%s=&lt;-' %s
 
 DESCRIPTION
-    add <a href='{3}'>?&lt;lang&gt;</a> to resulting url for line numbers and syntax highlighting
-    use <a href='{2}'>this form</a> to paste from a browser
+    add <a href='http://pygments.org/docs/lexers/'>?&lt;lang&gt;</a> to resulting url for line numbers and syntax highlighting
+    use <a href='%s'>this form</a> to paste from a browser
 
 EXAMPLES
-    ~$ cat bin/ching | curl -F '{0}=&lt;-' {1}
-       {1}/aXZI
-    ~$ firefox {1}/aXZI?py#n-7
+    ~$ cat tmp | curl -F '%s=&lt;-' %s
+       %s/VZiY
+    ~$ firefox %s/VZiY?py#n-7
 
 SEE ALSO
-    http://github.com/rupa/sprunge
+    <a href="http://github.com/anchoo2kwl/sprunge">http://github.com/anchoo2kewl/sprunge</a>
 
-</pre>""".format(POST, URL, form, '<a href="http://pygments.org/docs/lexers/"')
+</pre>""" % (r, u, f, r, u, u, u)
 
-def make_blob(nid, data):
-    filename = '/{0}/{1}'.format(get_default_gcs_bucket_name(), nid)
-    with gcs.open(
-        filename, 'w', content_type='text/plain; charset=UTF-8'
-    ) as fh:
-        fh.write(data.encode('utf-8'))
-    blobstore_filename = '/gs{0}'.format(filename)
-    return blobstore.create_gs_key(blobstore_filename)
+    def new_id(self):
+        nid = ''
+        symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        while len(nid) < 4:
+            n = random.randint(0,35)
+            nid = nid + symbols[n:n+1]
+        return nid
 
-class Sprunge(db.Model):
-    name = db.StringProperty()
-    content = db.TextProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
-    blob = db.StringProperty()
-
-class MainHandler(webapp2.RequestHandler):
-
-    def get(self):
-        self.response.out.write('''
-        <html>
-        <body>
-        {0}
-        </body>
-        </html>
-        '''.format(help()))
-
-    def post(self):
-        nid = new_id()
-        while Sprunge.gql('WHERE name = :1', nid).get():
-            nid = new_id()
-        s = Sprunge()
-        s.name = nid
-        key = make_blob(nid, self.request.get(POST))
-        s.blob = key
-        try:
-            s.put()
-        except Exception as ex:
-            self.response.out.write('{0}\n'.format(ex))
-            logging.error(ex)
+    def get(self, got):
+        if not got:
+            self.response.out.write(self.help(self.u, self.r))
             return
-        self.response.out.write('{0}/{1}\n'.format(URL, nid))
 
-class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+        # deleting
+        #if got.endswith('/secretpassword'):
+        #    got = got.split('/')
+        #    c = Sprunge.gql('WHERE name = :1', got[0]).get()
+        #    self.response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
+        #    if c:
+        #        self.response.out.write('''
+        #        deleting %s
+        #        --------
+        #        c.content
+        #        ''' % (got[0], c.content))
+        #        c.delete()
+        #    else:
+        #        self.response.out.write(got[0] + ' not here')
+        #    return
 
-    def get(self, resource):
-        resource = str(urllib.unquote(resource))
-
-        c = Sprunge.gql('WHERE name = :1', resource).get()
+        c = Sprunge.gql('WHERE name = :1', got).get()
         if not c:
-            self.response.out.write('{0} not found.'.format(resource))
+            self.response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
+            self.response.out.write(got + ' not found')
             return
-
-        # See if we need to migrate to Blobstore.
-        try:
-            data = c.blob
-        except AttributeError:
-            data = c.content
-        else:
-            if c.blob:
-                data = blobstore.BlobReader(c.blob).read()
-            else:
-                data = c.content
-                # migrate
-                logging.info(
-                    'Migrating {0} from Datastore to Blobstore.'
-                    .format(resource)
-                )
-                key = make_blob(resource, data)
-                c.blob = key
-                c.content = ''
-                try:
-                    c.put()
-                except Exception as ex:
-                    logging.error(ex)
 
         syntax = self.request.query_string
         if not syntax:
             self.response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
-            self.response.out.write(data + '\n')
+            self.response.out.write(c.content + '\n')
             return
-
         try:
             lexer = pygments.lexers.get_lexer_by_name(syntax)
         except:
             lexer = pygments.lexers.TextLexer()
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.out.write(highlight(
-            data,
-            lexer,
-            HtmlFormatter(
-                full=True,
-                style='borland',
-                lineanchors='n',
-                linenos='inline',
-                encoding='latin-1' # weird, but this works and utf-8 does not.
-            )
-        ))
+        self.response.out.write(highlight(c.content,
+                                          lexer,
+                                          HtmlFormatter(full=True,
+                                          style='borland',
+                                          lineanchors='n',
+                                          linenos='inline',
+                                          encoding='utf-8')))
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/([^/]+)?', ServeHandler)],
-    debug=False,
-)
+    def post(self, got):
+        self.response.headers['Content-Type'] = 'text/plain'
+        got = self.request.query_string
+        if self.request.get(self.r):
+            nid = self.new_id()
+            while Sprunge.gql('WHERE name = :1', nid).get():
+                nid = self.new_id()
+            s = Sprunge()
+            s.content = self.request.get(self.r)
+            s.name = nid
+
+            s.put()
+            self.response.out.write(' ' + self.u + '/' + nid + '\n')
+
+def main():
+    application = webapp.WSGIApplication([(r'/(.*)', Index)],debug=False)
+    wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+    main()
